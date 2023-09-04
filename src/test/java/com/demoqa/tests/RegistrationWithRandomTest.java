de9a7766765aa567e562a6af98d40a104ee1911a
Sergey Petyukov <petyukovsv@gmail.com>
2023-09-04 17:03:25 +0300

    jenkins

--- src/test/java/com/demoqa/tests/RegistrationWithRandomTest.java
+++ src/test/java/com/demoqa/tests/RegistrationWithRandomTest.java
@@ -1,40 +1,47 @@
 package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationPage;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
 
 import static com.demoqa.utils.TestDataProperties.*;
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationWithRandomTest extends BaseTest {
     RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
     void fakerFillFormTest() {
-        registrationPage.openPage().executeBanner().
-                setFirstName(firstNameAdd).
-                setLastName(lastNameAdd).
-                setUserEmail(eMailAdd).
-                setGender(gengerAdd).
-                setNumber(numberPhoneAdd).
-                setBirthDate(randomMonth, dayAdd, yearAdd).
-                setSubject(subjectAdd).
-                setHobbies(hobbiesAdd).
-                addPicture(picturesAdd).
-                setAddress(addressAdd).
-                setState(state).
-                setCity(cityAdd).
-                clickBottom();
-
-        registrationPage.
-                checkResult("Student Name", firstNameAdd + " " + lastNameAdd).
-                checkResult("Student Email", eMailAdd).
-                checkResult("Gender", gengerAdd).
-                checkResult("Mobile", numberPhoneAdd).
-                checkResult("Date of Birth", dayAdd + " " + randomMonth + "," + yearAdd ).
-                checkResult("Subjects", subjectAdd).
-                checkResult("Hobbies", hobbiesAdd).
-                checkResult("Picture", picturesAdd).
-                checkResult("Address", addressAdd).
-                checkResult("State and City", state + " " + cityAdd);
+        step("Отрытие страницы", () -> {
+        registrationPage.openPage().executeBanner();
+        });
+        step("Заполнение формы регистрации", () -> {
+                    registrationPage.setFirstName(firstNameAdd).
+                            setLastName(lastNameAdd).
+                            setUserEmail(eMailAdd).
+                            setGender(gengerAdd).
+                            setNumber(numberPhoneAdd).
+                            setBirthDate(randomMonth, dayAdd, yearAdd).
+                            setSubject(subjectAdd).
+                            setHobbies(hobbiesAdd).
+                            addPicture(picturesAdd).
+                            setAddress(addressAdd).
+                            setState(state).
+                            setCity(cityAdd).
+                            clickBottom();
+                });
+        step("Проверка заполненных полей формы регистрации", () -> {
+            registrationPage.
+                    checkResult("Student Name", firstNameAdd + " " + lastNameAdd).
+                    checkResult("Student Email", eMailAdd).
+                    checkResult("Gender", gengerAdd).
+                    checkResult("Mobile", numberPhoneAdd).
+                    checkResult("Date of Birth", dayAdd + " " + randomMonth + "," + yearAdd).
+                    checkResult("Subjects", subjectAdd).
+                    checkResult("Hobbies", hobbiesAdd).
+                    checkResult("Picture", picturesAdd).
+                    checkResult("Address", addressAdd).
+                    checkResult("State and City", state + " " + cityAdd);
+        });
     }
 }
